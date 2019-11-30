f1dbdfca7c0dd780fb0cc8cf42d0ec1fac098116
Moises Loarte <moises.loartec@gmail.com>
2019-11-29 20:53:00 -0500

    commit 7

--- app/src/main/java/app/android/frisco/myeasyfield/activities/activities/CanchasActivity.java
+++ app/src/main/java/app/android/frisco/myeasyfield/activities/activities/CanchasActivity.java
@@ -1,9 +1,18 @@
 package app.android.frisco.myeasyfield.activities.activities;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
 import app.android.frisco.myeasyfield.R;
+import app.android.frisco.myeasyfield.activities.entities.Canchas;
+import app.android.frisco.myeasyfield.activities.entities.Complejo;
+import app.android.frisco.myeasyfield.activities.repository.CanchasRepository;
+import app.android.frisco.myeasyfield.activities.repository.GeneratedRepository;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +20,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class CanchasActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Spinner distrito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +35,33 @@ public class CanchasActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        distrito = findViewById(R.id.distrito);
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        distrito.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mMap.clear();
+                String item = parent.getSelectedItem().toString();
+                List<Complejo> canchas = CanchasRepository.findByDistrict(item, getApplicationContext());
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                for(Complejo c : canchas){
+                    LatLng sydney = new LatLng(c.getLatitud(), c.getLongitud());
+                    mMap.addMarker(new MarkerOptions().position(sydney).title(c.getName()));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/app/android/frisco/myeasyfield/activities/repository/CanchasRepository.java
@@ -0,0 +1,26 @@
+package app.android.frisco.myeasyfield.activities.repository;
+
+import android.content.Context;
+import android.widget.Toast;
+import app.android.frisco.myeasyfield.activities.entities.Canchas;
+import app.android.frisco.myeasyfield.activities.entities.Complejo;
+import com.orm.SugarRecord;
+
+import java.util.List;
+
+public class CanchasRepository {
+
+    public static List<Complejo> findByDistrict(String distrito, Context context){
+        List<Complejo> complejos = SugarRecord.find(Complejo.class,"DISTRICT = '"+distrito+"'");
+        List<Complejo> todos = SugarRecord.listAll(Complejo.class);
+        if (complejos.isEmpty()){
+            Toast.makeText(context, "Lo siento, no encontramos complejos registrados en ese distrito. Se le " +
+                    "mostrará todos los complejos nuevamente", Toast.LENGTH_LONG).show();
+            return todos;
+        }else{
+            return complejos;
+        }
+
+    }
+
+}
